a7a733e776d2ac31941fe0c158cd6c913871478a
Maria Wedler <maria.wedler@tietoevry.com>
2025-02-11 13:52:28 +0100

    User menu creted

---
+++ user_menu.py
@@ -0,0 +1,6 @@
+import math
+
+while True:
+    print("\n Choose the math operation. \n\n0 - Addition\n1 - Substration\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 - Square root\n7 - Logarithm\n8 - Sine\n9 - Cosine\n10 - Tangent \n")
+
+    oper = input("\n Your option from the menu: ")
